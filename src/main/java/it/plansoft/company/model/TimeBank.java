47d8460e7db0de0f297990309ae513d912d94656
LORYMAX <lorymax4@gmail.com>
2021-02-26 10:44:44 +0100

    Creazione entità TimeBank

---
+++ src/main/java/it/plansoft/company/model/TimeBank.java
@@ -0,0 +1,79 @@
+package it.plansoft.company.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TimeBank extends IDModel<Long> {
+
+	private Long idEmployee;
+	private int totSalary;
+	private int maxHoursForDay;
+	private int hoursWorked;
+	private int extraHours;
+	private int idLevel;
+	private int nPromotions;
+
+	public TimeBank() {
+	}
+
+	public TimeBank(Long idEmployee, int idLevel) {
+		this.idEmployee = idEmployee;
+		this.idLevel = idLevel;
+	}
+
+	public Long getIdEmployee() {
+		return idEmployee;
+	}
+
+	public void setIdEmployee(Long idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+
+	public int getTotSalary() {
+		return totSalary;
+	}
+
+	public void setTotSalary(int totSalary) {
+		this.totSalary = totSalary;
+	}
+
+	public int getMaxHoursForDay() {
+		return maxHoursForDay;
+	}
+
+	public void setMaxHoursForDay(int maxHoursForDay) {
+		this.maxHoursForDay = maxHoursForDay;
+	}
+
+	public int getHoursWorked() {
+		return hoursWorked;
+	}
+
+	public void setHoursWorked(int hoursWorked) {
+		this.hoursWorked = hoursWorked;
+	}
+
+	public int getExtraHours() {
+		return extraHours;
+	}
+
+	public void setExtraHours(int extraHours) {
+		this.extraHours = extraHours;
+	}
+
+	public int getIdLevel() {
+		return idLevel;
+	}
+
+	public void setIdLevel(int idLevel) {
+		this.idLevel = idLevel;
+	}
+
+	public int getnPromotions() {
+		return nPromotions;
+	}
+
+	public void setnPromotions(int nPromotions) {
+		this.nPromotions = nPromotions;
+	}
+}
